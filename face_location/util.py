ae1d6eaccc8fe334e2ab6025394578500ca8db7c
zhaozhy145 <109942298+zhaozhy145@users.noreply.github.com>
2022-08-23 13:55:47 +0800

    Add files via upload

--- face_location/util.py
+++ face_location/util.py
@@ -1,13 +1,18 @@
 import cv2 as cv
 import emotion_distinguish_locate.util
 import joblib
+import tensorflow as tf
+import numpy as np
 
 # 读入多人脸图片
-IMAGE_PATH = '../Data/103884.jpg'
+IMAGE_PATH = '../Data/bfd6de4743a6276c5c9d565027f71e5c1660362029024.jpeg'
 image_read = cv.imread(IMAGE_PATH)
+wrong_num = {6: 4, 5: 2, 4: 6, 3: 3, 2: 5, 1: 0, 0: 1}
 LABEL_DICT = {
     0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'
 }
+
+# image_read = cv.resize(image_read, (650, 650))
 
 
 def get_face(image):
@@ -38,17 +43,28 @@ def cut_face_from_image(initial_image, faces):
 def emotion_sign(model, image):
     predict_image = []
     faces = get_face(image)
-    image_face = cut_face_from_image(image, faces)
-    for i in image_face:
-        image_p = emotion_distinguish_locate.util.preprocess_read_image(i)
-        image_p = emotion_distinguish_locate.util.unify_image(image_p)
-        image_p = emotion_distinguish_locate.util.normalize_data(image_p)
-        predict_image.append(image_p)
-
-    predict = model.predict(predict_image)
-    for i in range(len(predict)):
-        cv.putText(image, LABEL_DICT[predict[i]], (faces[i][0], faces[i][1]), fontFace=0, color=[0, 0, 255],
-                   thickness=1,
-                   fontScale=1)
+    if len(faces) > 0:
+        image_face = cut_face_from_image(image, faces)
+        for i in image_face:
+            image_p = emotion_distinguish_locate.util.preprocess_read_image(i)
+            image_p = emotion_distinguish_locate.util.unify_image(image_p)
+            image_p = emotion_distinguish_locate.util.normalize_data(image_p)
+            predict_image.append(image_p)
+        predict_image = tf.constant(predict_image)
+        predict = model.predict(predict_image)
+        for i in range(len(predict)):
+            cv.putText(image, LABEL_DICT[wrong_num[np.argmax(predict[i])]], (faces[i][0], faces[i][1]), fontFace=0,
+                       color=[0, 255, 255],
+                       thickness=int(image.shape[0] / 300),
+                       fontScale=image.shape[0] / 500)
+            cv.rectangle(image, (faces[i][0], faces[i][1]), (faces[i][0] + faces[i][2], faces[i][1] + faces[i][3]),
+                         (0, 0, 255), int(image.shape[0] / 600))
 
     return image
+
+
+#model = tf.keras.models.load_model('../emotion_distinguish_locate/model/FINAL_CNN_MODEL')
+#image = emotion_sign(model, image_read)
+#cv.imshow('test', image)
+#cv.waitKey()
+#cv.destroyAllWindows()
