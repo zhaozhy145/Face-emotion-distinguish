3acc38a86a2ccdf84f4c7c92e009360c8ed6a46f
zhaozhy145 <109942298+zhaozhy145@users.noreply.github.com>
2022-08-23 13:56:13 +0800

    Add files via upload

--- multi_face_sign.py
+++ multi_face_sign.py
@@ -1,9 +1,17 @@
 from face_location.util import emotion_sign
 import tensorflow as tf
 import cv2 as cv
-model = tf.keras.models.load_model('emotion_distinguish_locate/model/cnn_model0')
-image = cv.imread('Data/103884.jpg')
-image = emotion_sign(model, image)
-cv.imshow('test', image)
-cv.waitKey()
-cv.destroyAllWindows()
+
+
+# model = tf.keras.models.load_model('emotion_distinguish_locate/model/FINAL_CNN_MODEL')
+# image = cv.imread('Data/_20220823091945.jpg')
+# image = cv.resize(image, (570, 300))
+# image = emotion_sign(model, image)
+# cv.imshow('test', image)
+# cv.waitKey()
+# cv.destroyAllWindows()
+
+def solute(image):
+    model = tf.keras.models.load_model('../emotion_distinguish_locate/model/FINAL_CNN_MODEL')
+    image_f = emotion_sign(model, image)
+    return image_f
